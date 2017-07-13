3eafa97bdae04c0ba1364d81737fdb74c59582d7
cholical <ronding@live.unc.edu>
2017-07-13 00:12:03 -0700

    First commit

---
+++ public/app/home/home.controller.js
@@ -0,0 +1,30 @@
+(function () {
+
+	'use strict';
+	var app;
+
+	app = angular.module('starter');
+	app.controller('homeCtrl', ['$scope', '$state', 'homeSvc', '$geolocation', function homeCtrl($scope, $state, homeSvc, $geolocation){
+		$geolocation.getCurrentPosition({
+            timeout: 60000
+        }).then(function(position) {
+            $scope.myPosition = position;
+            console.log(position.coords);
+            $scope.center = {
+	            lat: $scope.myPosition.coords.latitude,
+	            lng: $scope.myPosition.coords.longitude,
+	            zoom: 16
+	        }
+	        $scope.markers = {
+	            currentLocation: {
+	                lat: $scope.myPosition.coords.latitude,
+	                lng: $scope.myPosition.coords.longitude,
+	                message: "We are here!",
+	                focus: true,
+	                draggable: true
+	            }
+        	}
+        });
+        
+	}]);
+}());
